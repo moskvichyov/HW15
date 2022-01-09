b04149977ad32363dd37179229449d6d7b7d4127
moskvichyov <moskvichyov.p@gmail.com>
2022-01-09 09:12:12 +0300

    int<idx>, del err

--- main.py
+++ main.py
@@ -9,23 +9,21 @@ app = Flask(__name__)
 def load_data(query, db):
     with sqlite3.connect(db) as connection:
         cursor = connection.cursor()
-        cursor.row_factory = sqlite3.Row
-        try:
-            result = cursor.execute(query)
-            result = cursor.fetchall()
-            result = [dict(ix) for ix in result]
-            if len(result) > 0:
-                return result
-            else:
-                return "нет данных для вывода"
-        except (sqlite3.error, sqlite3.warning) as er:
-            return "error"
+        # cursor.row_factory = sqlite3.Row
+        result = cursor.execute(query)
+        result = cursor.fetchone()
+        result = [dict(ix) for ix in result]
+        if len(result) > 0:
+            return result
+        else:
+            return "нет данных для вывода"
 
 
-@app.route('/animals/<idx>')
+@app.route('/animals/<int:idx>')
 def run_data(idx):
     query = f"""select *
-                   From animal_norm left join outcomes o on animal_norm.animal_id = o.animal_id
+                   From animal_norm 
+                   left join outcomes on animal_norm.animal_id = outcomes.animal_id
                    where animal_norm.id={idx}
                    """
     result = load_data(query, DB_PATH)
